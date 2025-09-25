3c494601ba69156e505b80cee2b551eba0e31fbf
Theus-Rocha <matheus.r.silva27@aluno.senai.br>
2025-09-24 23:18:35 -0300

    Criar CRUD de usuários

--- obj/Debug/net8.0/Exo.WebApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/Exo.WebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+467e60e46a1eb05cc9e2679f9b20fde0d1048ad3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
